7be8012330b5e573610c587215ffece73c5c631f
wen <945193029@qq.com>
2018-11-26 18:49:10 +0800

    SE-

--- keras_retinanet/bin/train.py
+++ keras_retinanet/bin/train.py
@@ -309,7 +309,7 @@ def parse_args(args):
 
     parser.add_argument('--backbone',         help='Backbone model used by retinanet.', default='resnet50', type=str)
     parser.add_argument('--batch-size',       help='Size of the batches.', default=16, type=int)
-    parser.add_argument('--gpu',              help='Id of the GPU to use (as reported by nvidia-smi).',default='6,7')
+    parser.add_argument('--gpu',              help='Id of the GPU to use (as reported by nvidia-smi).',default='2,3')
     parser.add_argument('--multi-gpu',        help='Number of GPUs to use for parallel processing.', type=int, default=2)
     parser.add_argument('--multi-gpu-force',  help='Extra flag needed to enable (experimental) multi-gpu support.', action='store_true',default=True)
     parser.add_argument('--epochs',           help='Number of epochs to train.', type=int, default=50)
